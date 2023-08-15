431ee1f7642a1cb844d4ac501e13552ce01d510e
liamthetaylor <141086754+liamthetaylor@users.noreply.github.com>
2023-08-15 12:16:40 +1200

    Add files via upload

---
+++ Getting_input.py
@@ -0,0 +1,9 @@
+username = input("What's your name? ")
+fav_num = int(input("Favourite number? "))
+print("Hello {}, your favourite number is {}".format(username, fav_num))
+double = fav_num * 2
+print("{} doubled is {}".format(fav_num, double))
+half = fav_num / 2
+print("{} halved is {}".format(fav_num, half))
+square = fav_num * fav_num
+print("{} squared is {}".format(fav_num, square))
